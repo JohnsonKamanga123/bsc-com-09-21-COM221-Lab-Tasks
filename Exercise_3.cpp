9e6bfd97e94a6f86cd8b1e1cdb3d11d72c1b0d84
JohnsonKamanga123 <bsc-com-09-21@unima.ac.mw>
2024-04-18 18:36:04 +0200

    completed exercise 3

---
+++ Exercise_3.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <iterator>
+
+using namespace std;
+
+int main(){
+
+int sum [] {1,2,3,4,5,6};
+    string list [] = {"B123",
+                        "C234",
+                        "A345",
+                        "C15",
+                        "B177",
+                        "G3003",
+                        "C235",
+                        "B179"};
+    //length of array
+    int length = sizeof(list)/sizeof(list[0]);                    
+    for(int i = 0 ; i < length  ; i++){
+        if(list[i][0] == 'B') 
+            cout << "Index " << i << " has " << list[i][0] << endl;
+
+    }
+}
