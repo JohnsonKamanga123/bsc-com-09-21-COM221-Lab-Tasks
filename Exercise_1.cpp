a881f6041b39bba5050957b5b9e93e32d379f9b7
JohnsonKamanga123 <bsc-com-09-21@unima.ac.mw>
2024-04-19 03:07:53 +0200

    second method of obtaining random integers

--- Exercise_1.cpp
+++ Exercise_1.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
-#include <cstdlib>
+#include <random>
 
 using namespace std;
 
 int main(){
     unsigned int range = 11;
-    srand(range);
-    int daysUntilEpiration = rand();
+    random_device engine;
+    uniform_int_distribution<int> randomNumbers(1,range);
+    int daysUntilEpiration ;
 
     for(int i = 0 ; i < 11 ; i++)
-        cout << i << ". random number generated: " << rand()<< endl;  
+        cout << i << ". random number generated: "<< randomNumbers(engine) << endl;  
 
 }
