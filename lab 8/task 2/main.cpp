68128383443bf5e36e4c629ef1a6bf031ba3eda2
JohnsonKamanga123 <bsc-com-09-21@unima.ac.mw>
2024-05-10 09:02:03 +0200

    used the overloaaded == and != operators from the person class in the main function

--- lab 8/task 2/main.cpp
+++ lab 8/task 2/main.cpp
@@ -12,5 +12,13 @@ int main(){
 
     cout << "The total weight: " << totalWeight << endl;
 
+    if(Jane == John){
+        cout << "This is the same person" << endl;
+    }
+    
+    if(Jane != John){
+        cout << "This is NOT the same person" << endl;
+    }
+
     return 0;
 }
