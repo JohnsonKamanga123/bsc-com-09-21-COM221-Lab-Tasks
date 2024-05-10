3efea71c05c09206f3d1dfa39e743d5dee71001d
JohnsonKamanga123 <bsc-com-09-21@unima.ac.mw>
2024-05-10 07:53:19 +0200

    Created the Box header file and implementation file

---
+++ lab 8/task 1/Box.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <iostream>
+
+class Box{
+    public:
+
+    Box();
+
+    Box(const double newLength, const double newBreadth, const double newHeight);
+
+    ~Box();
+
+    double GetVolume();
+
+    void SetLength(double len);
+
+    void SetBreadth(double bre);
+
+    void SetHeight(double hei);
+
+    //overload + operator to add two objects
+    Box operator+(const Box& b);
+
+    private:
+    double length;
+    double breadth;
+    double height;
+
+};
