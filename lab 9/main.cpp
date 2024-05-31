60178a03e4124444e4f78ae7995c160e267ff4e5
JohnsonKamanga123 <bsc-com-09-21@unima.ac.mw>
2024-05-31 23:31:04 +0200

    added the virtual functions to the polygon class and modified main

--- lab 9/main.cpp
+++ lab 9/main.cpp
@@ -7,11 +7,15 @@ using namespace std;
 int main(){
     Rectangle rect;
     Triangle trgl;
+    Polygon poly;
     Polygon* pPoly1 = &rect;
     Poly* pPoly2 = &trgl;
+    Poly* pPoly3 = &poly;
     pPoly1 -> SetValues(4,5);
     pPoly2 -> SetValues(4,5);
-    cout << rect.Area() << '\n';
-    cout << trgl.Area() << '\n';
+    pPoly3 -> Setvalues(4,5);
+    cout << pPoly1 -> Area() << '\n';
+    cout << pPoly2 -> Area() << '\n';
+    cout << pPoly3 -> Area() << '\n';
     return 0;
 }
