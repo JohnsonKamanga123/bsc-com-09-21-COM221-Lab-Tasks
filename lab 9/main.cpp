612692384db2c2b642202090c02ebcef23046612
JohnsonKamanga123 <bsc-com-09-21@unima.ac.mw>
2024-05-31 23:27:00 +0200

    modified main function for rectangles and traingles by including base class pointers

--- lab 9/main.cpp
+++ lab 9/main.cpp
@@ -7,8 +7,10 @@ using namespace std;
 int main(){
     Rectangle rect;
     Triangle trgl;
-    rect.SetValues(4,5);
-    trgl.SetValues(4,5);
+    Polygon* pPoly1 = &rect;
+    Poly* pPoly2 = &trgl;
+    pPoly1 -> SetValues(4,5);
+    pPoly2 -> SetValues(4,5);
     cout << rect.Area() << '\n';
     cout << trgl.Area() << '\n';
     return 0;
